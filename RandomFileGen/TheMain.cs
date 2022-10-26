aed8119f93d69d92c6d8152b5e7410841dfe6f7c
Travis Brooks <traviscodes@gmail.com>
2022-10-25 20:47:43 -0700

    Add project files.

---
+++ RandomFileGen/TheMain.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RandomFileGen
+{
+    internal class TheMain
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TODO: write the damn program...");
+        }
+    }
+}
