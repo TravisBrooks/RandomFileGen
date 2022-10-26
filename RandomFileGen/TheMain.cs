71eecfa673c6119bf3f5ee2453235a9d053a8a06
Travis Brooks <traviscodes@gmail.com>
2022-10-25 22:34:17 -0700

    My initial implementation of this, its a simple idea so its a simple little console app

--- RandomFileGen/TheMain.cs
+++ RandomFileGen/TheMain.cs
@@ -1,12 +1,54 @@
 ﻿using System;
+using System.IO;
 
 namespace RandomFileGen
 {
     internal class TheMain
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("TODO: write the damn program...");
+            const string usageStr = "Usage: RandomFileGen <fileName> <fileSizeInBytes>";
+
+            if (args.Length != 2)
+            {
+                Console.Error.WriteLine("Error: requires exactly 2 parameters.");
+                Console.Error.WriteLine(usageStr);
+                return 1;
+            }
+
+            var fileName = args[0];
+            if (!long.TryParse(args[1], out long fileSizeInBytes))
+            {
+                Console.Error.WriteLine("Error: the second parameter must parse as a long numeric type.");
+                Console.Error.WriteLine(usageStr);
+                return 1;
+            }
+
+            if (fileSizeInBytes <= 0)
+            {
+                Console.Error.WriteLine("Error: the fileSizeInBytes parameter must be a positive number greater than 0.");
+                Console.Error.WriteLine(usageStr);
+                return 1;
+            }
+
+            long bytesWritten = 0;
+            var rand = new Random();
+            var bite = new byte[1];
+            using (var fstream = File.Open(fileName, FileMode.OpenOrCreate, FileAccess.Write))
+            {
+                using (var bufStream = new BufferedStream(fstream, 1024))
+                {
+                    while (bytesWritten < fileSizeInBytes)
+                    {
+                        rand.NextBytes(bite);
+                        bufStream.Write(bite);
+                        bytesWritten += bite.Length;
+                    }
+                    bufStream.Flush();
+                }
+            }
+
+            return 0;
         }
     }
 }
